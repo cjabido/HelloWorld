ee6ebe200abcc2160657a8efb1e4a9e08e7f107d
Carl <cjabido@gmail.com>
2016-07-09 16:16:24 -0400

    New Idea Project

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Carl on 7/9/2016. Yeah yeah it's a template. Who cares.
+ *
+ */
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
